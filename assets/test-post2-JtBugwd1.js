5d9b8d657c4243883c4cbe6d3ba6e7d51ec3e116
Davian Albarran <dalbarran.dev@gmail.com>
2024-11-23 16:11:41 -0500

    Updates

--- assets/test-post2-JtBugwd1.js
+++
@@ -1,7 +0,0 @@
-const t={title:"Test Post 2",date:"2024-03-24T00:00:00.000Z",excerpt:"This is a test post to verify blog functionality",tags:["test"]},o=`<h1>Test Post</h1>
-<p>This is a <em>test post</em> to verify that our blog system is working correctly.</p>
-<ol>
-<li>Hello</li>
-<li>World</li>
-</ol>
-`,s=[{level:"1",content:"Test Post"}];export{t as attributes,o as html,s as toc};
